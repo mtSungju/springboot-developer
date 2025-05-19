d9f72fcfdbdb76536f157b3a3dbda8db96a2fac3
sungju96 <tjdwn0320@naver.com>
2025-05-19 16:55:57 +0900

    first commit

---
+++ src/main/java/me/shinsungyoung/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,11 @@
+package me.shinsungyoung.springbootdeveloper;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args){
+        run(SpringBootDeveloperApplication.class, args);
+    }
+}
