1b7dc6a138e352ca96a35f22aa0086b453ea09f8
choisungju <tjdwn0320@naver.com>
2025-08-13 09:26:55 +0900

    stream 예제

--- src/main/java/me/shinsungyoung/Main.java
+++ src/main/java/me/shinsungyoung/Main.java
@@ -1,17 +1,41 @@
 package me.shinsungyoung;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+        List<String> fruits = Arrays.asList("apple", "banana", "apple", "orange", "kiwi", "watermelon");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
-        }
+        // a로 시작하는 과일만 뽑기
+        fruits.stream()
+                .filter(fruit -> fruit.startsWith("a"))
+                .forEach(System.out::println);
+
+        // 과일 이름 대문자로 변경하기
+        fruits.stream()
+                .map(fruit -> fruit.toUpperCase())
+                .forEach(System.out ::println);
+
+        // 정렬
+        fruits.stream()
+                .sorted()
+                .forEach(System.out::println);
+
+        Map<String,Integer> map = new HashMap<>();
+        map.put("A",1);
+        map.put("B",2);
+        map.put("C",3);
+        map.put("D",4);
+
+        map.entrySet().stream()
+                .filter(m->m.getValue()>1)
+                .forEach(m ->
+                        System.out.println(m.getKey() + " : " + m.getValue())
+                );
     }
 }
